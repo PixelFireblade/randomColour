08039b40222a932e2d718f13fe46f458147b249d
PixelFireblade <joshuamoses@outlook.com>
2017-04-16 15:46:08 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
 
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+$ ( document ).ready(function(){
+  $("#findNumber").click(function() {
+   
+    var no1 = Math.floor(Math.random() * (255 - 1 + 1)) + 1;
+    var no2 = Math.floor(Math.random() * (255 - 1 + 1)) + 1;
+    var no3 = Math.floor(Math.random() * (255 - 1 + 1)) + 1;
+    var color = alert( rgbToHex(no1, no2, no3) );
+    document.body.style.backgroundColor = color;
+    
+    console.log(randomNumber);
+    $("p").text(randomNumber);
+ })
+});
