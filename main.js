b7b8a575d86ce7e64ad761440e8de5926fe8b10d
JCosmos <joshuamoses@outlook.com>
2019-02-01 18:17:32 +1100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $ ( document ).ready(function(){
     var c = Math.floor(Math.random() * (255 - 1 + 1)) + 1;
    
     
-    $("body").css("background-color: " + "rgb(+"a+","+b+","+c"+"));")
+    $("body").css("background-color: " + "rgb(+"a+","+b+","+c"+"));");
     $("#deployColour").append("rgb(+"a+","+b+","+c"+"));");
 
 
